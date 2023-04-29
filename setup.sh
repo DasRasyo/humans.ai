bd225194490e12e862f4ecf751160728a815fd73
Conqueror <94050636+DasRasyo@users.noreply.github.com>
2023-04-29 05:06:50 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+
+echo -e "                                                       ";
+echo -e "   ______                                              ";
+echo -e "  / ____/___  ____  ____ ___  _____  _________  _____  ";
+echo -e " / /   / __ \/ __ \/ __  / / / / _ \/ ___/ __ \/ ___/  ";
+echo -e "/ /___/ /_/ / / / / /_/ / /_/ /  __/ /  / /_/ / /      ";
+echo -e "\____/\____/_/ /_/\__  /\__ _/\___/_/   \____/_/       ";
+echo -e "                    /_/                                ";
+echo -e "                                                       ";
+
+echo -e "\033[38;5;245mTwitter : https://twitter.com/Conquerorr_1\033[0m"
+echo -e "\033[38;5;245mGithub  : https://github.com/DasRasyo\033[0m"
+echo -e "\033[38;5;205mHumans AI Node\033[0m"
+
+sleep 8
+
+prompt() {
+  read -p "$1: " val
+  echo $val
+}
+
+
+
+echo -e "\033[38;5;205m⚠️Starting with Packages update and Dependencies Inslall⚠️"
+sleep 5
+
+sudo apt update && apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential \
+git make ncdu -y 
+
+sleep 5
+
+echo -e "\033[38;5;205m⚠️Installing GO⚠️\033[0m"
+
+sleep 10
+
+cd $HOME
+curl -Ls https://go.dev/dl/go1.20.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+touch $HOME/.bash_profile
+source $HOME/.bash_profile
+PATH_INCLUDES_GO=$(grep "$HOME/go/bin" $HOME/.bash_profile)
+if [ -z "$PATH_INCLUDES_GO" ]; then
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+  echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
+fi
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+sleep 5
+
+source $HOME/.bash_profile
+
+echo -e "\033[38;5;205mPackages updated, Go and Dependencies Inslalled. You can check your go version with = go version\033[0m"
+
+sleep 7
+
+echo -e "\033[38;5;205mDownloading the Binary\033[0m"
+
+sleep 10
+
+git clone https://github.com/humansdotai/humans
+cd humans
+git checkout v0.1.0
+make install
+
+humansd config keyring-backend test
+humansd config chain-id humans_3000-1
+validator_node_name=$(prompt "Enter your validator node name")
+humansd init $validator_node_name --chain-id humans_3000-1 
+
+sleep 10
+
+curl -s https://raw.githubusercontent.com/humansdotai/testnets/master/friction/genesis.json > $HOME/.humansd/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/humans-testnet/addrbook.json > $HOME/.humansd/config/addrbook.json
+
+SEEDS="3f472746f46493309650e5a033076689996c8881@humans-testnet.rpc.kjnodes.com:22659"
+PEERS="ca1e46048e4a9b65d60bc9e749aa431401f34ed7@144.76.45.59:26656,8c3c25fa9cd6289d0342cf0e42916f381a1fd671@78.46.64.59:26656,62faee4c6224b3562d7123acea58180021c8b47b@162.55.173.57:26656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:22656,34fd880f1bf2eb7b7481436d0746bbc05a4a5805@144.76.97.35:26656,ceba57f1376d4949cc0419918d110f0085b24b25@135.181.113.225:26656,7acb02817755961e446859eef57c494687e3330f@162.244.35.40:26656,4762fa22edb91acd78010026f8da5fb71e174acb@142.165.207.19:36656,c94e42eec8bcc7f6db81748a97e5f10d59710e95@135.181.138.160:26656,497886715ac23475f7428bd177b9fa53ff886a8d@78.46.80.79:26656"
+
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.humansd/config/config.toml
+
+
+
+echo -e "\033[38;5;205mStarting Service\033[0m"
+
+sleep 5
+
+sudo tee /etc/systemd/system/humansd.service > /dev/null << EOF
+[Unit]
+Description=Humans AI Node
+After=network-online.target
+[Service]
+User=root
+ExecStart=$(which humansd) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+[Install]
+WantedBy=multi-user.target
+EOF
+
+humansd tendermint unsafe-reset-all --home $HOME/.humansd --keep-addr-book
+
+sudo systemctl daemon-reload
+sudo systemctl enable humansd
+sudo systemctl start humansd
+
+sleep 5
+
+echo -e "\033[38;5;205mCongrats!! Your node started!\033[0m"
+sleep 7
+echo -e "\033[38;5;205mSome Useful Command That You May Need For Service. Copy and Save!\033[0m"
+sleep 3
+echo -e "\033[38;5;205mCheck Your Logs:   sudo journalctl -u humansd -f --no-hostname -o cat\033[0m"
+sleep 3
+echo -e "\033[38;5;205mStop the Service:   sudo systemctl stop humansd\033[0m"
+sleep 3
+echo -e "\033[38;5;205mStart the Service:   sudo systemctl start humansd\033[0m"
+sleep 3
+echo -e "\033[38;5;205mRestart the Service:   sudo systemctl restart humansd\033[0m"
+sleep 3
+echo -e "\033[31m⚠️Before creating validator make sure you fully synced!!!!⚠️\033[0m"
+sleep 5
+
+
+echo -e "\033[31m⚠️⚠️⚠️Lets create a wallet! Please do not forget to save your mnemonics!!!. If you dont save you can not access your wallet. After creating wallet, you will have 100 second to save your mnemonics. After that script will continued!⚠️⚠️⚠️\033[0m"
+echo -e "\033[31m⚠️ Before creating your validator do not forget to top up your wallet with some testnet coin! ⚠️\033[0m"
+echo -e "\033[31m⚠️⚠️⚠️ SAVE THE MNEMONICS⚠️⚠️⚠️\033[0m"
+sleep 17
+
+wallet_name=$(prompt "Enter your account-wallet name")
+humansd keys add $wallet_name
+
+sleep 100
+
+echo -e "\033[38;5;205mWith this script we automaticly check if your node fully synced. After synced you can go on with creating your validator. The script will check sync status every 60 seconds and will print the status.\033[0m"
+
+while true
+do
+
+    sync_status=$(curl -s localhost:26657/status | jq '.result | .sync_info | .catching_up')
+        if [ "$sync_status" = "false" ]; then
+        echo "Your node is synced with the Humans network."
+            sleep 5
+            echo "Your node is now synced with the Humans network. Proceed with validator creation."
+            sleep 5
+            echo "Stop the script with ctrl C and edit the following command with your information to create your validator!"
+            sleep 10
+            echo -e "\033[38;5;205mhumansd tx staking create-validator --amount=1000000000000000000aheart --pubkey=$(humansd tendermint show-validator) --moniker=$validator_node_name --chain-id=humans_3000-1 --commission-rate=0.05 --commission-max-rate=0.10 --commission-max-change-rate=0.01 --min-self-delegation=1000000 --gas=auto --gas-prices=1800000000aheart --from=$wallet_name\033[0m"
+		sleep 20
+
+        else
+       echo "Your node is not synced with the Humans network. Waiting for sync to complete..."
+           sleep 60
+        fi
+done
